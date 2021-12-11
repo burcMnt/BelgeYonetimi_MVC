3c4fdc14d84f9eda2328c5fe582b0f1d776cf8fb
burcMnt <burcumantar0@gmail.com>
2021-12-11 17:41:03 +0300

    Add project files.

---
+++ BelgeYonetimi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BelgeYonetimi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
