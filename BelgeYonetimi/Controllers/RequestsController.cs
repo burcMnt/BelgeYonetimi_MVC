c77a951853901f947554d157a35f9456cb761d32
burcMnt <burcumantar0@gmail.com>
2021-12-12 05:42:03 +0300

    RequestController and Uploaded File part

---
+++ BelgeYonetimi/Controllers/RequestsController.cs
@@ -0,0 +1,66 @@
+﻿using BelgeYonetimi.Data;
+using BelgeYonetimi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BelgeYonetimi.Controllers
+{
+    [Authorize]
+    public class RequestsController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnv;
+        private readonly ApplicationDbContext _db;
+
+        public RequestsController(IHostingEnvironment hostingEnv, ApplicationDbContext db)
+        {
+            _hostingEnv = hostingEnv;
+            _db = db;
+        }
+        public IActionResult NewRequest()
+        {
+            return View();
+        }
+
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult NewRequest(UserRequest userRequest,RequestVM requestVM )
+        {
+            
+
+            if (requestVM.File != null)
+            {
+                var fileName = Path.GetFileName(requestVM.File.FileName);
+                //judge if it is pdf file
+                string ext = Path.GetExtension(requestVM.File.FileName);
+                if (ext.ToLower() != ".pdf")
+                {
+                    return View();
+                }
+
+                var filePath = Path.Combine(_hostingEnv.WebRootPath, "documents", fileName);
+
+                using (var fileSteam = new FileStream(filePath, FileMode.Create))
+                {
+                    requestVM.File.CopyToAsync(fileSteam);
+                }
+                //your logic to save filePath to database, for example
+
+               
+                    userRequest.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                    userRequest.Document = filePath;
+                    _db.UserRequests.Add(userRequest);
+                    _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ BelgeYonetimi/Models/RequestVM.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelgeYonetimi.Models
+{
+    public class RequestVM
+    {
+        [Required, MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string UserLastName { get; set; }
+        public string Explanation { get; set; }
+
+        [Required]
+        public string Document { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
